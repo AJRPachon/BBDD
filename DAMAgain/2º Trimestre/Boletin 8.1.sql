e6cf84e87b0cf734bc01d3ae44a0b19d78eb20ad
AJRPachon <49155621+AJRPachon@users.noreply.github.com>
2020-01-17 14:40:43 +0100

    Update Boletin 8.1.sql

--- DAMAgain/2º Trimestre/Boletin 8.1.sql
+++ DAMAgain/2º Trimestre/Boletin 8.1.sql
@@ -3,20 +3,45 @@ GO
 
 --1. Nombre del país y número de clientes de cada país, ordenados alfabéticamente por el nombre del país.
 
-	
+	SELECT * FROM Customers
 
+	SELECT Country, COUNT(CustomerID) AS [Numero de clientes por pais] FROM Customers
+	GROUP BY Country
+	ORDER BY Country
 
 --2. ID de producto y número de unidades vendidas de cada producto. 
+
+	SELECT * FROM [Order Details]
+
+	SELECT ProductID, COUNT(Quantity) AS [Numero unidades vendidas] FROM [Order Details]
+	GROUP BY ProductID
 
 
 --3. ID del cliente y número de pedidos que nos ha hecho.
 
+	SELECT * FROM Orders
+
+	SELECT CustomerID, COUNT(CustomerID) AS [Numero de pedidos realizados]  FROM Orders
+	WHERE CustomerID != 'NULL'
+	GROUP BY CustomerID
+	
 
 --4. ID del cliente, año y número de pedidos que nos ha hecho cada año.
+
+	SELECT * FROM Orders
+	
+	SELECT CustomerID, YEAR(OrderDate) AS Año, COUNT(YEAR(OrderDate)) AS [Numero de pedidos cada año] FROM Orders 
+	WHERE CustomerID != 'NULL'
+	GROUP BY CustomerID, YEAR(OrderDate)
+	ORDER BY CustomerID, YEAR(OrderDate)
 
 
 --5. ID del producto, precio unitario y total facturado de ese producto, ordenado por cantidad facturada de mayor a menor. Si hay varios precios unitarios para el mismo producto tomaremos el mayor.
 
+	SELECT * FROM [Order Details]
+
+	SELECT ProductID, UnitPrice, SUM(UnitPrice*Quantity) AS [Total facturado] FROM [Order Details]
+	GROUP BY ProductID, UnitPrice
 
 --6. ID del proveedor e importe total del stock acumulado de productos correspondientes a ese proveedor.
 
