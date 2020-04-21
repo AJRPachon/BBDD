441c7f901336a07b89c3a9008028f2f32c69e09f
AJRPachon <49155621+AJRPachon@users.noreply.github.com>
2020-04-21 17:49:57 +0200

    Anunciados boletines

---
+++ DAMAgain/3º Trimestre/Boletin 11.0.sql
@@ -0,0 +1,29 @@
+
+USE LeoMetro
+GO
+
+--1.- Crea una función inline que nos devuelva el número de estaciones que ha recorrido cada tren en un determinado periodo de tiempo. El principio y el fin de ese periodo se pasarán como parámetros
+
+
+--2.- Crea una función inline que nos devuelva el número de veces que cada usuario ha entrado en el metro en un periodo de tiempo. El principio y el fin de ese periodo se pasarán como parámetros
+
+
+--3.- Crea una función inline a la que pasemos la matrícula de un tren y una fecha de inicio y fin y nos devuelva una tabla con el número de veces que ese tren ha estado en cada estación, además del ID, 
+--  nombre y dirección de la estación
+
+
+--4.- Crea una función inline que nos diga el número de personas que han pasado por una estacion en un periodo de tiempo. Se considera que alguien ha pasado por una estación si ha entrado o salido del metro por ella.
+--  El principio y el fin de ese periodo se pasarán como parámetros
+
+
+--5.- Crea una función inline que nos devuelva los kilómetros que ha recorrido cada tren en un periodo de tiempo. El principio y el fin de ese periodo se pasarán como parámetros
+
+
+--6.- Crea una función inline que nos devuelva el número de trenes que ha circulado por cada línea en un periodo de tiempo. El principio y el fin de ese periodo se pasarán como parámetros.
+--  Se devolverá el ID, denominación y color de la línea
+
+
+--7.- Crea una función inline que nos devuelva el tiempo total que cada usuario ha pasado en el metro en un periodo de tiempo. El principio y el fin de ese periodo se pasarán como parámetros. 
+--  Se devolverá ID, nombre y apellidos del pasajero. El tiempo se expresará en horas y minutos.
+
+
